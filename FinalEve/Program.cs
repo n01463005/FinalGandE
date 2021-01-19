6cc2aaa3bc6d5b62ad96feb4ece27d27cf1479c0
Daniel <Daniel.baslios@gmail.com>
2021-01-18 19:53:10 -0500

    Initial Commit

---
+++ FinalEve/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace FinalEve
+{
+  public class Program
+    {
+        static void Main(string[] args)
+        {
+            Service service = new Service() { Oilchange = "Change the oil pls" };
+            Car car = new Car();
+            var serviceCar = new ServiceCar();
+            serviceCar.OilService += car.OnService;
+            serviceCar.Change(service);
+        }
+    }
+    public class ServiceEventArgs : EventArgs
+    {
+        public Service service { get; set; }
+    }
+
+    public class ServiceCar
+    {
+        public event EventHandler<ServiceEventArgs> OilService;
+        public void Change(Service oil)
+        {
+            Console.WriteLine("Oil change is due");
+            OnService(oil);
+        }
+        protected virtual void OnService(Service oil)
+        {
+            if (OilService != null)
+                OilService(this, new ServiceEventArgs() { service = oil });
+        }
+    }
+    public class Service
+    {
+        public string Oilchange { get; set; }
+    }
+    public class Car
+    {
+        public void OnService(object source, ServiceEventArgs e)
+        {
+            Console.WriteLine("Car Sercice:" + e. service.Oilchange);
+        }
+    }
+
+}
